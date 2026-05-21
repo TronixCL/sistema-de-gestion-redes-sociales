ddf845537cb115ae2bc1cf091a2aab1ad7692f79
CR1SOL1NY <cristobal.saavedrav@alumnos.uv.cl>
2026-05-20 21:49:33 -0400

    Refactor data curation into a function
    Refactored data processing into a function and added input parameter for file name.

--- tareaEstructura/estructuraDatos.py
+++ tareaEstructura/estructuraDatos.py
@@ -1,33 +1,35 @@
 import os
 
-directorio_actual = os.path.dirname(os.path.abspath(__file__))
-ruta_entrada = os.path.join(directorio_actual, 'instagram_data.csv')
-ruta_salida = os.path.join(directorio_actual, 'dataset_curado.csv')
+def curarDatos(nombreEntrada:str):
+    directorio_actual = os.path.dirname(os.path.abspath(__file__))
+    ruta_entrada = os.path.join(directorio_actual, nombreEntrada)
+    ruta_salida = os.path.join(directorio_actual, 'dataset_curado.csv')
 
-columnas_a_usar = [0, 1, 2, 13]
-
-with open(ruta_entrada, mode='r', encoding='utf-8') as archivo:
-    lineas = archivo.readlines()
-
-datos_curados = []
-
-for linea in lineas:
-    print(linea.strip())
+    with open(ruta_entrada, mode='r', encoding='utf-8') as archivo:
+        lineas = archivo.readlines()
     
-    if not linea.strip():
-        continue
+    datos_curados = []
+
+    for linea in lineas: 
+       
+        if not linea.strip():
+            continue
+            
+        parametros = linea.strip().split(',')
         
-    parametros = linea.strip().split(',')
-    
-    if len(parametros) < 14:
-        parametros.extend([''] * (14 - len(parametros)))
-    elif len(parametros) > 14:
-        parametros = parametros[:14]
+        if len(parametros) == 14 and parametros[0] :
+            columnas_a_usar = [0,1, 2, 13]
         
-    fila_filtrada = [parametros[i] for i in columnas_a_usar]
-    datos_curados.append(','.join(fila_filtrada) + '\n')
+            fila_filtrada = [parametros[i] for i in columnas_a_usar]
+            fila_unida = ','.join(fila_filtrada)
+            datos_curados.append(fila_unida + '\n')
+        else: 
+            continue
+        
+        
+    print(datos_curados)    
 
-    
+    with open(ruta_salida, mode='w', encoding='utf-8') as archivo_salida:
+        archivo_salida.writelines(datos_curados)
 
-with open(ruta_salida, mode='w', encoding='utf-8') as archivo_salida:
-    archivo_salida.writelines(datos_curados)
+curarDatos('instagram_data.csv')
